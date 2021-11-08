98a2a83106661e0423f096a057dc0f1928fbaa55
cynthia manurung <54532326+cynthiamanurung@users.noreply.github.com>
2021-11-08 11:58:15 +0700

    Add files via upload

---
+++ Users.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <Users />
+  </div>
+</template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    name: 'Users',
+    data() {
+      return {
+        users: null,
+      };
+    },
+    created: function() {
+      axios
+        .get('https://jsonplaceholder.typicode.com/users')
+        .then(res => {
+          this.users = res.data;
+        })
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
